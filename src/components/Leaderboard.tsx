264744bdb07d614e3d965d6125a12e89524536d8
JariSleurink <225001@edu.rocfriesepoort.nl>
2023-10-30 10:54:47 +0100

    Leaderboard component

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,17 @@
+import '../App.css'
+import LeaderboardCard from './LeaderboardCard'
+
+const Leaderboard = () => {
+    return(
+        <div className="leaderboard">
+            <h1 className="Leaderboard"></h1>
+            <LeaderboardCard />
+            <LeaderboardCard />
+            <LeaderboardCard />
+
+        </div>
+
+    )
+}
+
+export default Leaderboard
