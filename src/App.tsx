c64a21d43d505cf02d7f04999c62cec3ca8c1d6e
SleepyTK <75376380+SleepyTK@users.noreply.github.com>
2023-11-02 09:08:09 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,7 @@ function App() {
 
   return (
     <>
-      <div className="leaderboard">
-        <h1 className='title'>LEADERBOARD</h1>
-        {
           <Leaderboard />
-          /* <LeaderboardCard />
-        <LeaderboardCard />
-        <LeaderboardCard /> */}
-      </div>
     </>
   )
 }
--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -4,7 +4,7 @@ import LeaderboardCard from './LeaderboardCard'
 const Leaderboard = () => {
     return(
         <div className="leaderboard">
-            <h1 className="Leaderboard"></h1>
+            <h1 className='title'>LEADERBOARD</h1>
             <LeaderboardCard />
             <LeaderboardCard />
             <LeaderboardCard />
