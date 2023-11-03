76f72c81ecc9d2a35811df7bf44b45f53c17c0a0
quickrick2 <rick.kampstra2002@gmail.com>
2023-11-03 12:44:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import './App.css'
 import Leaderboard from './components/Leaderboard'
-import LeaderboardCard from './components/LeaderboardCard'
 import ScanComponent from './components/ScanComponent'
 
 function App() {
 
   return (
     <>
-      <div className="leaderboard">
-        <h1 className='title'>LEADERBOARD</h1>
-        {
+
+        
           <Leaderboard />
-          /* <LeaderboardCard />
-        <LeaderboardCard />
-        <LeaderboardCard /> */}
-      </div>
+          
+
       <ScanComponent />
     </>
   )
