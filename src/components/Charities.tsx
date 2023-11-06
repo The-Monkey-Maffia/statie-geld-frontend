bc6482a7115862db17c037ccb125d47072e68060
Xaethier <jannes.dalstra@gmail.com>
2023-11-06 10:46:18 +0100

    Formatted idk

--- src/components/Charities.tsx
+++ src/components/Charities.tsx
@@ -6,7 +6,7 @@ const Leaderboard = () => {
     { photo: "https://shaolin-vechtkunst.nl/wp-content/uploads/2016/03/kika-logo.jpg" },
     { photo: "https://www.copman.be/project/wp-content/uploads/WWF-226x300-1.jpg" },
     { photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGKagyXMMuaWyBHeN3iGlOvOOF1c7EVE3ziQ&usqp=CAU" },
-    { photo: "https://media.discordapp.net/attachments/951432745605795841/1170796123913470022/EUtm70sU0AIVfiS.png?ex=655a57dc&is=6547e2dc&hm=4ba43292b53952798a35293144c68333194c193dda3acba910549067b07f50eb&=&width=483&height=515" }
+    { photo: "https://i.natgeofe.com/n/82fddbcc-4cbb-4373-bf72-dbc30068be60/drill-monkey-01_2x3.jpg" }
   ];
 
   return (
