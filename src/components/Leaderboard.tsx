a548c23354c667ddfc2d8cb5371a4d8eeefd0284
Fredkr0ket <ferdovbalen@gmail.com>
2023-11-09 10:15:39 +0100

    mapped leaderboardcard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const Leaderboard = () => {
 
-  const [apiData, setData] = useState<any>('')
+  const [apiData, setData] = useState<[{aantal_votes: number, id: number, info: string, link: string, name: string}]>()
 
   // const leaderboardData = [
   //   { name: "Kika", score: 100, description: "Hulp voor kinderen van kanker", placement: 1 },
@@ -23,10 +23,12 @@ const Leaderboard = () => {
     // const apiData = data()
     // console.log(apiData);
     axios.get('http://localhost:3000/get/goededoel/')
-    .then((response) => setData(response.data))
-    console.log('apiData: ', apiData);
+    .then((response) => {
+      setData(response.data.data)
+      console.log(response.data.data)
+    })
 
-  })
+  }, [])
 
 
 
@@ -34,18 +36,12 @@ const Leaderboard = () => {
     <div className="leaderboard">
       <h1 className='title'>Leaderboard</h1>
       {
-        apiData.map((charity: any, index: number) => {
-          <LeaderboardCard key={index} name={charity.name} description={charity.description} score={charity.score} placement={charity.placement} />
+        apiData?.map((charity: {aantal_votes: number, id: number, info: string, link: string, name: string}, index: number) => {
+          return (
+            <LeaderboardCard key={index} name={charity.name} description={charity.info} score={charity.aantal_votes} placement={69} />
+          )
         })
       }
-      {
-      /* {leaderboardData.map((charity, index) => (
-        <LeaderboardCard key={index} name={charity.name} description={charity.description} score={charity.score} placement={charity.placement} />
-      ))} */}
-      {/* {data && <LeaderboardCard data={data} />} */}
-      {
-        
-      }      
     </div>
   );
 };
