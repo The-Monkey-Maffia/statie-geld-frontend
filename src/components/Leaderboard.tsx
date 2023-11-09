a18ebc45ba2c5580834ed009cacac63299f6e5ee
SleepyTK <75376380+SleepyTK@users.noreply.github.com>
2023-11-09 08:59:42 +0100

    leaderboard changes

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -4,12 +4,12 @@ import LeaderboardCard from './LeaderboardCard';
 import axios from 'axios';
 
 const Leaderboard = () => {
-  // const leaderboardData = [
-  //   { name: "Kika", score: 100, description: "Hulp voor kinderen van kanker", placement: 1 },
-  //   { name: "WWF", score: 85, description: "World Wide Fond voor de natuur", placement: 2 },
-  //   { name: "Dierenlot", score: 120, description: "Stichting voort dieren", placement: 3 },
-  //   // Voeg meer spelers toe...
-  // ];
+  const leaderboardData = [
+    { name: "Kika", score: 100, description: "Hulp voor kinderen van kanker", placement: 1 },
+    { name: "WWF", score: 85, description: "World Wide Fond voor de natuur", placement: 2 },
+    { name: "Dierenlot", score: 120, description: "Stichting voort dieren", placement: 3 },
+    // Voeg meer spelers toe...
+  ];
   
   useEffect(() => {
     axios.get('http://localhost:3000/get/goededoel/')
@@ -21,9 +21,9 @@ const Leaderboard = () => {
   return (
     <div className="leaderboard">
       <h1 className='title'>Leaderboard</h1>
-      {/* {leaderboardData.map((charity, index) => (
+      {leaderboardData.map((charity, index) => (
         <LeaderboardCard key={index} name={charity.name} description={charity.description} score={charity.score} placement={charity.placement} />
-      ))} */}
+      ))}
       {/* {data && <LeaderboardCard data={data} />} */}      
     </div>
   );
