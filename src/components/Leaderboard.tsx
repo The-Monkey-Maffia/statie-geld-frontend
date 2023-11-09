32a8cde593aeaffd62314f774b3ea186e7b8a3ae
JariSleurink <225001@edu.rocfriesepoort.nl>
2023-11-09 09:52:47 +0100

    Error update Ferdo

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../App.css';
 import LeaderboardCard from './LeaderboardCard';
 import axios from 'axios';
 
 const Leaderboard = () => {
+
+  const [apiData, setData] = useState<any>('')
+
   // const leaderboardData = [
   //   { name: "Kika", score: 100, description: "Hulp voor kinderen van kanker", placement: 1 },
   //   { name: "WWF", score: 85, description: "World Wide Fond voor de natuur", placement: 2 },
@@ -12,19 +15,37 @@ const Leaderboard = () => {
   // ];
   
   useEffect(() => {
+    // const data = async() => {
+    //   const data = await axios.get('http://localhost:3000/get/goededoel/')
+    //   return data;
+    // }
+
+    // const apiData = data()
+    // console.log(apiData);
     axios.get('http://localhost:3000/get/goededoel/')
-    .then((response) => {
-      console.log(response.data);
-    })
+    .then((response) => setData(response.data))
+    console.log('apiData: ', apiData);
+
   })
+
+
 
   return (
     <div className="leaderboard">
       <h1 className='title'>Leaderboard</h1>
-      {/* {leaderboardData.map((charity, index) => (
+      {
+        apiData.map((charity: any, index: number) => {
+          <LeaderboardCard key={index} name={charity.name} description={charity.description} score={charity.score} placement={charity.placement} />
+        })
+      }
+      {
+      /* {leaderboardData.map((charity, index) => (
         <LeaderboardCard key={index} name={charity.name} description={charity.description} score={charity.score} placement={charity.placement} />
       ))} */}
-      {/* {data && <LeaderboardCard data={data} />} */}      
+      {/* {data && <LeaderboardCard data={data} />} */}
+      {
+        
+      }      
     </div>
   );
 };
