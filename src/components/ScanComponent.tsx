b3bf477bbcf2b0f9a2546bf847558e3e3c369759
Fredkr0ket <ferdovbalen@gmail.com>
2023-11-10 16:04:21 +0100

    changed scan component to work but not with new scanned stuf

--- src/components/ScanComponent.tsx
+++ src/components/ScanComponent.tsx
@@ -1,41 +1,62 @@
-import { useEffect } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+async function getBarcodeTitle(barcode: string) {
+    const response = await axios.get(`http://localhost:3000/get/drinks/${barcode}`);
+    return response.data.data[0].name;
+}
 
 function ScanComponent() {
-    const scannedCodes: string[] = [];
+    const [scannedCodes, setScannedCodes] = useState<string[]>(["3254381062561", "3254381062561", "3254381062561"]);
+    const [titles, setTitles] = useState<string[]>([]);
+
     let barcode: string = '';
 
-    const handleKeyPress = (event: KeyboardEvent) => {
-        // console.log('Key pressed:', event.key);
-
+    const handleKeyPress = async (event: KeyboardEvent) => {
         if (event.key === 'Enter') {
             console.log('Barcode scanned:', barcode);
             scannedCodes.push(barcode);
+            setScannedCodes([...scannedCodes]);
             barcode = ''; // Clear the barcode string
             console.log(scannedCodes);
         } else {
             barcode = barcode + event.key;
         }
-
-        // Perform actions based on the key pressed
     };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const promises = scannedCodes.map(async (data) => {
+                const title = await getBarcodeTitle(data);
+                return title;
+            });
+
+            const titles = await Promise.all(promises);
+            setTitles(titles);
+        };
+
+        fetchData();
+    }, [scannedCodes]);
 
     useEffect(() => {
         const handleKeyPressEvent = (event: KeyboardEvent) => {
             handleKeyPress(event);
         };
 
-        // Add event listener when the component mounts
         document.addEventListener('keydown', handleKeyPressEvent);
 
-        // Remove event listener when the component unmounts
         return () => {
             document.removeEventListener('keydown', handleKeyPressEvent);
         };
-    }, []); // Empty dependency array ensures this effect runs only once (on mount)
+    }, [handleKeyPress]);
 
     return (
         <div>
-            {/* Content of your component */}
+            <ul>
+                {titles.map((title, index) => (
+                    <li key={index}>{title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
