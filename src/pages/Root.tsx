3dddf233412dab66a15887858def9120ac99bfad
quickrick2 <rick.kampstra2002@gmail.com>
2023-11-10 15:09:16 +0100

    Fata Morgana

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,13 +1,10 @@
 import Leaderboard from "../components/Leaderboard"
-import Products from "../components/Products"
 import ScanComponent from "../components/ScanComponent"
 
 function Root() {
   return (
     <>
     <Leaderboard />
-
-    <Products />
 
     <ScanComponent />
     </>
